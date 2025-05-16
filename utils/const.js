dbdb03f04d6dc76c7090fb3c035228465896837d
Abhishek Kumar <mintu12890551@gmail.com>
2025-05-16 16:44:41 +0530

    url

--- utils/const.js
+++ utils/const.js
@@ -1,4 +1,4 @@
 // export const BACKEND_URL = "https://together-server-five.vercel.app/";
-// export const BACKEND_URL = "https://together-server-0if8.onrender.com";
+export const BACKEND_URL = "https://together-server-0if8.onrender.com";
 // export const BACKEND_URL = "http://192.168.29.106:5000";
-export const BACKEND_URL = "http://192.168.31.221:5000";
+// export const BACKEND_URL = "http://192.168.31.221:5000";
